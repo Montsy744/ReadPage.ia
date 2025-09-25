f0b1264d0ff11291f207d2b3ba4268f5023cefb2
Hugo Straseele <straseelehugo@gmail.com>
2025-09-25 16:36:29 +0200

    ajout du début de la gestion des session grace aux cookies

--- readpage.ia/app/[...link]/page.tsx
+++ readpage.ia/app/[...link]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Chat from "../components/Chat";
+import { cookies } from "next/headers";
 
 interface pageProps {
   params : {
@@ -12,7 +13,9 @@ function reconstructUrl({url} : {url : string[]}) {
   return decodedComponents.join('/');
 }
 
-const Page = ({ params }: pageProps) => {
+const Page = async ({ params }: pageProps) => {
+
+  const sessionCookies = (await cookies()).get('sessionId')?.value;
   
   if (!params.link || params.link.length === 0) {
     return <div>Erreur : Aucun lien fourni</div>;
@@ -21,6 +24,7 @@ const Page = ({ params }: pageProps) => {
   const linkArray = Array.isArray(params.link) ? params.link : [params.link];; // si c'est un tableau, on le garde sinon on le met dans un tableau
   const decodedLink = reconstructUrl({url : linkArray}); //on reconstruit l'url
 
+  const sessionId = (decodedLink + "__" + sessionCookies).replace(/\//g, "");
   return (
     <Chat decodedLink={decodedLink}></Chat>
   )
